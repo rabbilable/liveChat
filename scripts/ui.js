0dad4f13f68057aee7555b04d89d2d3900f44a16
rabbilable <rabbilyasar@gmail.com>
2019-08-05 21:23:46 +0600

    finish chat app

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,6 +5,11 @@ class ChatUI {
   constructor(list){
     this.list = list;
   }
+  
+  clear(){
+    this.list.innerHTML = '';
+  }
+
   render(data){
     const when = dateFns.distanceInWordsToNow(
       data.created_at.toDate(),
